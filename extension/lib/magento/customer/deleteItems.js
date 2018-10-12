19f78b1a40125f07cbf22f1a736255daa3ee3b52
Rainer Skistims <rainer.skistims@shopgate.com>
2018-10-12 15:45:46 +0200

    MAGENTO-2111 add delete items pipeline

--- extension/lib/magento/customer/deleteItems.js
+++ extension/lib/magento/customer/deleteItems.js
@@ -3,9 +3,16 @@ const MagentoRequest = require('../Request')
 /**
  * @param {Object} context
  * @param {Object} input
- * @returns {Promise<{productIds: string[]}>}
  */
 module.exports = async (context, input) => {
+  const wishlistItemIdMapping = await context.storage.user.get('wishlistItemIdMapping')
+  const wishlistItemIds = input.productIds.map(productId => wishlistItemIdMapping[productId])
+  const wishlistItemIdsString = wishlistItemIds.toString()
+
+  if (!wishlistItemIdsString) {
+    return
+  }
+
   const request = new MagentoRequest(context, input.token)
   const wishlistIdsEndpointUrl = `${context.config.magentoUrl}/wishlists`
   /**
@@ -16,17 +23,10 @@ module.exports = async (context, input) => {
   const wishlists = await request.send(wishlistIdsEndpointUrl, 'getWishlistItems')
 
   if (!wishlists.length || !wishlists[0].wishlist_id) {
-    return { productIds: [] }
+    return
   }
 
-  const wishlistItemsEndpointUrl = `${context.config.magentoUrl}/wishlists/${wishlists[0].wishlist_id}/items`
-  const wishlistItems = await request.send(wishlistItemsEndpointUrl, 'Request to Magento: getFavorites', 'GET')
-  const wishlistItemIdMapping = {}
-  const productIds = wishlistItems.map(item => {
-    wishlistItemIdMapping[item.product_id] = item.wishlist_item_id
-    return item.product_id;
-  })
-  context.storage.user.set('wishlistItemIdMapping', wishlistItemIdMapping)
-
-  return { productIds }
+  const itemFilter = `?wishlistItemIds=${wishlistItemIdsString}`
+  const wishlistItemsEndpointUrl = `${context.config.magentoUrl}/wishlists/${wishlists[0].wishlist_id}/items${itemFilter}`
+  request.send(wishlistItemsEndpointUrl, 'Request to Magento: deleteFavorites', 'DELETE')
 }
