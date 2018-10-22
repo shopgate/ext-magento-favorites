5be9b6c2caa2f12047a1fad7b85f50ed19ae7115
Rainer Skistims <rainer.skistims@shopgate.com>
2018-10-22 16:54:13 +0200

    MAGENTO-2111 add id mapping to add step

--- extension/lib/magento/customer/addItems.js
+++ extension/lib/magento/customer/addItems.js
@@ -11,5 +11,11 @@ module.exports = async (context, input) => {
   const wishlistAddItemEndpointUrl = `${context.config.magentoUrl}/wishlists/${input.wishlistId}/items`
   const wishlistItems = input.transformedProducts.map(({ product }) => product)
 
-  await request.send(wishlistAddItemEndpointUrl, 'addFavorites', 'POST', wishlistItems)
+  const response = await request.send(wishlistAddItemEndpointUrl, 'addFavorites', 'POST', wishlistItems)
+  const wishlistItemIdMapping = await context.storage.user.get('wishlistItemIdMapping') || {}
+
+  Object.entries(response.wishlistItemIds).forEach(entry => {
+    wishlistItemIdMapping[entry[0]] = entry[1]
+  });
+  context.storage.user.set('wishlistItemIdMapping', wishlistItemIdMapping)
 }
