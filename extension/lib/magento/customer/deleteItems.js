b3a4a317a5c3897c19e57f39224908b20fced85a
Rainer Skistims <rainer.skistims@shopgate.com>
2018-10-23 10:40:01 +0200

    MAGENTO-2111 outsource getting of wishlist id

--- extension/lib/magento/customer/deleteItems.js
+++ extension/lib/magento/customer/deleteItems.js
@@ -15,18 +15,7 @@ module.exports = async (context, input) => {
   }
 
   const request = new MagentoRequest(context, input.token)
-  /**
-   * Get all wishlist ids of the customer. At the moment we only support one wishlist per customer
-   * @typedef {Object} wishlists
-   * @property {string} wishlist_id
-   */
-  const wishlists = await request.send(`${context.config.magentoUrl}/wishlists`, 'getWishlistItems')
-
-  if (!wishlists.length || !wishlists[0].wishlist_id) {
-    return
-  }
-
   const itemFilter = `?wishlistItemIds=${wishlistItemIdsString}`
-  const wishlistItemsEndpointUrl = `${context.config.magentoUrl}/wishlists/${wishlists[0].wishlist_id}/items${itemFilter}`
+  const wishlistItemsEndpointUrl = `${context.config.magentoUrl}/wishlists/${input.wishlistId}/items${itemFilter}`
   request.send(wishlistItemsEndpointUrl, 'Request to Magento: deleteFavorites', 'DELETE')
 }
