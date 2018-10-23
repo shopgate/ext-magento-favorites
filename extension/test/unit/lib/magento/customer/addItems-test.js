ca2c6a7a474fc0c5c3972701d98be924df83988f
Rainer Skistims <rainer.skistims@shopgate.com>
2018-10-23 12:05:43 +0200

    MAGENTO-2111 unit test for addItems

--- extension/test/unit/lib/magento/customer/addItems-test.js
+++ extension/test/unit/lib/magento/customer/addItems-test.js
@@ -1,8 +1,10 @@
 const assert = require('assert')
 const request = require('request-promise-native')
 const nock = require('nock')
+const sinon = require('sinon')
 
 const getWishlistId = require('../../../../../lib/magento/customer/getWishlistId')
+const addItems = require('../../../../../lib/magento/customer/addItems')
 const transformItemId = require('../../../../../lib/transformItemId')
 
 describe('magento/customer: addItems step', () => {
@@ -30,10 +32,18 @@ describe('magento/customer: addItems step', () => {
       log: {
         debug: (object, message) => {
         }
+      },
+      storage: {
+        user: {
+          get: () => {
+          },
+          set: () => {
+          }
+        }
       }
     }
   })
-  it('Should return the first existing wischlistId', async () => {
+  it('Should return the first existing wishlistId', async () => {
     nock(magentoUrl).get(path).reply(200, [{ wishlist_id: 2 }, { wishlist_id: 1 }])
     const response = await getWishlistId(context, input)
     assert.deepStrictEqual(response, { wishlistId: 2 })
@@ -48,5 +58,16 @@ describe('magento/customer: addItems step', () => {
     const result = await transformItemId(context, input)
     const resultItem = result.products.pop()
     assert.deepStrictEqual(resultItem, { id: '337', productId: '337' })
+  })
+  it('Should use the user storage', async () => {
+    const input = {
+      token: 'testToken',
+      wishlistId: '1',
+      transformedProducts: []
+    }
+    nock(magentoUrl).post('/wishlists/1/items').reply(200, {wishlistItemIds: ['1', '2']})
+    const requestSpy = sinon.spy(context.storage.user, 'get')
+    await addItems(context, input)
+    assert(requestSpy.called);
   })
 })
