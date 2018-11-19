5a629fe1ebc49e8da51adf1fb000a942eecca691
Alexander Wesselburg <info@myportal.de>
2018-11-19 12:34:25 +0100

    MAGENTO-2139 - Rename test file for syncItems and adjust tests

--- extension/test/unit/lib/magento/customer/getSyncItems-test.js
+++ extension/test/unit/lib/magento/customer/getSyncItems-test.js
@@ -12,26 +12,26 @@ describe('getSyncItems', () => {
 
   it('Should return the full list of products ids and correct array of product id(s) to add by adding one guest product', async () => {
     input = {
-      mageProductsIds: [{ id: 10 }],
-      guestProductIds: [{ id: 20 }]
+      mageProductsIds: [10],
+      guestProductIds: [20]
     }
 
-    const expectedAddResult = [{ id: 20 }]
-    const expectedProductIdsResult = [{ id: 10 }, { id: 20 }]
+    const expectedAddResult = [20]
+    const expectedProductIdsResult = [10, 20]
     const diffObject = await step(context, input)
 
     assert.deepStrictEqual(diffObject.addProductsIds, expectedAddResult)
     assert.deepStrictEqual(diffObject.productIds, expectedProductIdsResult)
   })
 
-  it('Should return the full list of products ids and a empty array of products id(s) to add because the product products are the same', async () => {
+  it(' f Should return the full list of products ids and a empty array of products id(s) to add because the product products are the same', async () => {
     input = {
-      mageProductsIds: [{ id: 20 }],
-      guestProductIds: [{ id: 20 }]
+      mageProductsIds: [20],
+      guestProductIds: [20]
     }
 
     const expectedAddResult = []
-    const expectedProductIdsResult = [{ id: 20 }]
+    const expectedProductIdsResult = [20]
     const diffObject = await step(context, input)
 
     assert.deepStrictEqual(diffObject.addProductsIds, expectedAddResult)
@@ -40,12 +40,12 @@ describe('getSyncItems', () => {
 
   it('Should return  the full list of products ids and a empty array of products id(s) to add because there are no guest product given', async () => {
     input = {
-      mageProductsIds: [{ id: 20 }, { id: 30 }],
+      mageProductsIds: [20, 30],
       guestProductIds: []
     }
 
     const expectedAddResult = []
-    const expectedProductIdsResult = [{ id: 20 }, { id: 30 }]
+    const expectedProductIdsResult = [20, 30]
     const diffObject = await step(context, input)
 
     assert.deepStrictEqual(diffObject.addProductsIds, expectedAddResult)
@@ -55,11 +55,11 @@ describe('getSyncItems', () => {
   it('Should return the full list of products ids and a correct array of products id(s) to add by empty mage products', async () => {
     input = {
       mageProductsIds: [],
-      guestProductIds: [{ id: 20 }, { id: 30 }]
+      guestProductIds: [20, 30]
     }
 
-    const expectedAddResult = [{ id: 20 }, { id: 30 }]
-    const expectedProductIdsResult = [{ id: 20 }, { id: 30 }]
+    const expectedAddResult = [20, 30]
+    const expectedProductIdsResult = [20, 30]
     const diffObject = await step(context, input)
 
     assert.deepStrictEqual(diffObject.addProductsIds, expectedAddResult)
@@ -68,12 +68,12 @@ describe('getSyncItems', () => {
 
   it('Should return the full list of products ids and correct array of products id(s) to add on merge', async () => {
     input = {
-      mageProductsIds: [{ id: 40 }],
-      guestProductIds: [{ id: 20 }, { id: 30 }]
+      mageProductsIds: [40],
+      guestProductIds: [20, 30]
     }
 
-    const expectedAddResult = [{ id: 20 }, { id: 30 }]
-    const expectedProductIdsResult = [{ id: 40 }, { id: 20 }, { id: 30 }]
+    const expectedAddResult = [20, 30]
+    const expectedProductIdsResult = [40, 20, 30]
     const diffObject = await step(context, input)
 
     assert.deepStrictEqual(diffObject.addProductsIds, expectedAddResult)
@@ -83,25 +83,25 @@ describe('getSyncItems', () => {
   it('Should return the full list of products ids and array of all guest products id(s) because there are no mage products', async () => {
     input = {
       mageProductsIds: [],
-      guestProductIds: [{ id: 20 }, { id: 30 }]
+      guestProductIds: [20, 30]
     }
 
-    const expectedAddResult = [{ id: 20 }, { id: 30 }]
-    const expectedProductIdsResult = [{ id: 20 }, { id: 30 }]
+    const expectedAddResult = [20, 30]
+    const expectedProductIdsResult = [20, 30]
     const diffObject = await step(context, input)
 
     assert.deepStrictEqual(diffObject.addProductsIds, expectedAddResult)
     assert.deepStrictEqual(diffObject.productIds, expectedProductIdsResult)
   })
 
-  it('Should return  the full list of products ids and array of all product id(s) to add becsuse there are no guest products given', async () => {
+  it('Should return the full list of products ids and array of all product id(s) to add becsuse there are no guest products given', async () => {
     input = {
-      mageProductsIds: [{ id: 20 }, { id: 30 }],
+      mageProductsIds: [20, 30],
       guestProductIds: []
     }
 
     const expectedAddResult = []
-    const expectedProductIdsResult = [{ id: 20 }, { id: 30 }]
+    const expectedProductIdsResult = [20, 30]
     const diffObject = await step(context, input)
 
     assert.deepStrictEqual(diffObject.addProductsIds, expectedAddResult)
